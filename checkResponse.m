4de3f5e72cbf5fffe524e7cb437f29d5ed2d91f9
alexmchughnz <alexmchughnz@gmail.com>
2018-04-25 14:18:09 +1200

    Moved checkResponse to own file.

---
+++ checkResponse.m
@@ -0,0 +1,22 @@
+function checkResponse(V, dV, y, r, VLim, dVLim, setTol)
+%checkResponse Prints response info.
+%Determines if control effort and settling meets criteria.
+    
+%Tests
+err = r - y(end);
+VPass = isempty(find(abs(V) > VLim, 1));
+dVPass = isempty(find(abs(dV) > dVLim, 1));
+setPass = abs(y(end) - r) < setTol;
+
+%Printed Results
+resp = ["BAD","OK"];
+fprintf("Error of %f [m] (%.1f%%)\n", ...
+        err, err/r*100);
+fprintf("Voltage %s. max = %.2f / %d [V]\n", ...
+        resp(VPass+1), max(abs(V)), VLim);
+fprintf("Slew Rate %s. max = %.2f / %d [V/s]\n", ...
+        resp(dVPass+1), max(abs(dV)), dVLim);
+fprintf("Settling %s. final = %.5f / %.3f [m]\n", ...
+        resp(setPass+1), y(end), r);
+
+end
