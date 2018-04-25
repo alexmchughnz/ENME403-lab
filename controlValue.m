9010b41abd6e1771c6c93bd195e352e7bdfd2ff3
alexm <alexm@192.168.1.110>
2018-04-25 13:08:35 +1200

    Added controlValue function.

---
+++ controlValue.m
@@ -0,0 +1,15 @@
+function [ u du ] = controlValue(x, K, N, r)
+%controlValue Computes control effort from state values over time.
+%Designed for a tracking system where u = -Kx + Nr.
+
+%OUTPUTS:
+% u - Control input.
+% du - (approx.) Rate of change. du(ii) = u(ii+1) - u(ii)
+
+u = -K*x' + N*r;
+
+du = u(1:end-1) - u(2:end);
+
+
+end
+
