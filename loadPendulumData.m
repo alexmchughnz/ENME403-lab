fb9adea1e7c28c5f55eaa9828a5d4fd223ec9cb2
Alex McHugh <adm181@uclive.ac.nz>
2018-05-09 16:03:23 +1200

    Added loadCartData and fixed pendulum script.

--- loadPendulumData.m
+++ loadPendulumData.m
@@ -1,16 +1,16 @@
 function data = loadPendulumData(filename)
     load(filename);
-    file = eval(filename)
+    file = eval(filename);
     vars = file.Y;
     
-    t = file.X.Data;    
+    t = file.X(1).Data;    
     r = vars(6).Data;
     x = vars(1).Data;
     xdot = vars(3).Data;
     theta = vars(8).Data;    
     thetadot = vars(9).Data;
-    K = [vars(2).Data(end), vars(4).Data(end)...
-         vars(7).Data(end), vars(10).Data(end)];
+    K = [vars(2).Data(end), vars(7).Data(end)...
+         vars(4).Data(end), vars(10).Data(end)];
     N = vars(5).Data(end);
     V = vars(11).Data;
     
@@ -22,20 +22,6 @@ function data = loadPendulumData(filename)
                   'thetadot', thetadot,...
                   'K', K,...
                   'N', N,...
-                  'V', V);
-    
-%     cart_position = example.Y(1).Data;
-%     cart_position_gain = example.Y(2).Data;
-%     cart_velocity = example.Y(3).Data;
-%     cart_velocity_gain = example.Y(4).Data;
-%     tracking_gain = example.Y(5).Data;
-%     cart_position_command = example.Y(6).Data;
-%     pendulum_position_gain = example.Y(7).Data;
-%     pendulum_position = example.Y(8).Data;
-%     pendulum_velocity = example.Y(9).Data;
-%     pendulum_velocity_gain = example.Y(10).Data;
-%     raw_motor_voltage = example.Y(11).Data;
-    
-    
+                  'V', V);    
 end
 
